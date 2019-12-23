0aac8505b862e925377d2a0aa3fac2f989973a15
Eu93n3 <fukasawaliberty@gmail.com>
2019-12-23 16:11:17 +0900

    餌やり機能を追加

---
+++ MLBoids_A/Assets/Script/bait.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bait : MonoBehaviour
+{
+  // Update is called once per frame
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Player")
+    {
+      gameObject.GetComponentInParent<GameManagement>().score++;
+      Destroy(gameObject);
+    }
+  }
+}

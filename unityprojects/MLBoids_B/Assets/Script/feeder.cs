79d2fd8ba1a4e15243762504f571a62ffd6abce5
Eu93n3 <fukasawaliberty@gmail.com>
2020-01-27 13:29:41 +0900

    a

---
+++ unityprojects/MLBoids_B/Assets/Script/feeder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feeder : MonoBehaviour
+{
+  [SerializeField]
+  GameObject baitprefab;
+
+  [SerializeField]
+  int amount = 0;
+
+  void AddBaits()
+  {
+    float lim = gameObject.GetComponent<GameManagement>().FieldSize / 2;
+    var go = Instantiate(baitprefab, new Vector3(Random.Range(-lim, lim), GameObject.FindWithTag("floor").transform.position.y+1,Random.Range(-lim, lim)), Random.rotation);
+    go.transform.SetParent(this.transform);
+    
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    for(int i = 0; i < amount; i++)
+    {
+      AddBaits();
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
---
+++ unityprojects/MoveCube_Ray/README.txt
@@ -0,0 +1,3 @@
+2次元探索のAIを好奇心(curiosity)を利用して学習しました。
+好奇心はml-agentsのサンプルのpyramidを参考にしました。
+学習の際にこのファイル内のtrainer_config.yamlを使用しました。
